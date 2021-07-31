c19fca98bc9fc1b489c42dbbb98f79d24e941acd
yogeshBsht <ybnsit@gmail.com>
2021-07-31 23:16:27 +0530

    Added 'email' attribute to class 'Book'

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class Book(UserMixin, db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     superuser = db.Column(db.Boolean)
     books_issued = db.relationship(
