4c57e3ce4f3dfb69c289a042e3e36e5d6a390824
yogeshBsht <ybnsit@gmail.com>
2021-08-14 13:05:04 +0530

    Added route for adding book to database

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
 from app import db
 from app.models import Book
 from app.main import bp
-from app.main.forms import EmptyForm, UpdateInventoryForm
+from app.main.forms import EmptyForm, UpdateInventoryForm, AddBookForm
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -23,7 +23,7 @@ def index():
 @bp.route('/update_inventory/<bookname>', methods=['GET', 'POST'])
 @login_required
 def update_inventory(bookname):
-    book = Book.query.filter_by(bookname=bookname).first()
+    books = Book.query.filter_by(bookname=bookname).first()
     form = UpdateInventoryForm()
     if form.validate_on_submit():
         book.update_inventory(form.inventory.data)
@@ -46,7 +46,8 @@ def issue(bookname):
     form = EmptyForm()
     if form.validate_on_submit():
         current_user.issue_book(book)
-        db.session.commit()        
+        db.session.commit()
+        flash(f'Book {bookname} issued')        
     return redirect(url_for('main.index'))
 
 
@@ -58,4 +59,18 @@ def returnn(bookname):
     if form.validate_on_submit():
         current_user.return_book(book)
         db.session.commit()   
+        flash(f'Book {bookname} returned')
     return redirect(url_for('main.index'))
+
+
+@bp.route('/add_book', methods=['GET','POST'])
+@login_required
+def add_book():
+    form = AddBookForm()
+    if form.validate_on_submit():
+        book = Book(bookname=form.bookname.data, author=form.author.data, inventory=form.inventory.data)
+        db.session.add(book)
+        db.session.commit()
+        flash(f'Congratulations, book {book.bookname} added to database')
+        return redirect(url_for('main.index'))
+    return render_template('addBook.html', title='AddBook', form=form)
