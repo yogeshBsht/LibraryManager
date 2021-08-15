ea23bfd2f522ab95ddd8efe47c047c702084d37d
yogeshBsht <ybnsit@gmail.com>
2021-08-15 12:37:13 +0530

    Added pagination functionality for viewing books

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request, \
+    current_app
 from flask_login import current_user, login_required
 from app import db
 from app.models import Book
@@ -10,14 +11,22 @@ from app.main.forms import EmptyForm, UpdateInventoryForm, AddBookForm
 @bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    book = Book.query.all()
+    page = request.args.get('page', 1, type=int)
+    book = Book.query.order_by(Book.id).paginate(
+        page, current_app.config['BOOKS_PER_PAGE'], False)
+    next_url = url_for('main.index', page=book.next_num) \
+            if book.has_next else None
+    prev_url = url_for('main.index', page=book.prev_num) \
+            if book.has_prev else None
     if current_user.username != 'admin':
         form = EmptyForm()
     else:
         form = UpdateInventoryForm()    
     if form.validate_on_submit():
-        db.session.commit()  
-    return render_template('index.html', title='Home', form=form, user=current_user, book=book)
+        db.session.commit()      
+    return render_template('index.html', title='Home', form=form, 
+            user=current_user, book=book.items, next_url=next_url,
+            prev_url=prev_url)
 
 
 @bp.route('/update_inventory/<bookname>', methods=['GET', 'POST'])
@@ -36,7 +45,8 @@ def update_inventory(bookname):
 def user():
     book = current_user.books_issued
     form = EmptyForm()
-    return render_template('index.html', title='MyBooks', user=current_user, form=form, book=book)
+    return render_template('index.html', title='MyBooks', user=current_user,
+            form=form, book=book)
 
 
 @bp.route('/issue/<bookname>', methods=['POST'])
@@ -68,7 +78,8 @@ def returnn(bookname):
 def add_book():
     form = AddBookForm()
     if form.validate_on_submit():
-        book = Book(bookname=form.bookname.data, author=form.author.data, inventory=form.inventory.data)
+        book = Book(bookname=form.bookname.data, author=form.author.data, 
+                inventory=form.inventory.data)
         db.session.add(book)
         db.session.commit()
         flash(f'Congratulations, book {book.bookname} added to database')
