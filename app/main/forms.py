5645fa0bb1c0fff14f106ea5c214b5fd79c92678
yogeshBsht <ybnsit@gmail.com>
2021-08-15 12:31:07 +0530

    Added method for validating bookname attribute

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Book
 
 
 class EmptyForm(FlaskForm):
@@ -16,4 +17,9 @@ class AddBookForm(FlaskForm):
     bookname = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     inventory = IntegerField('Inventory', validators=[DataRequired()])
-    submit = SubmitField('Add')   
+    submit = SubmitField('Add')  
+
+    def validate_bookname(self, bookname):
+        book = Book.query.filter_by(bookname=bookname.data).first()
+        if book is not None:
+            raise ValidationError(('Book already exists.')) 
