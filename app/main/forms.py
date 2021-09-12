76689d7f8708045eb5017d423b223e45cea37941
yogeshBsht <ybnsit@gmail.com>
2021-09-12 13:50:13 +0530

    Added method for validating inventory and SearchForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,21 +5,30 @@ from app.models import Book
 
 
 class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')  
+    submit = SubmitField('Submit')
 
 
 class UpdateInventoryForm(FlaskForm):
     inventory = IntegerField('Inventory', validators=[DataRequired()])
     submit = SubmitField(('Update'))
 
+    def validate_inventory(self, inventory):
+        if inventory.data<0:
+            raise ValidationError(('Inventory must be non-negative.'))
+
 
 class AddBookForm(FlaskForm):
     bookname = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     inventory = IntegerField('Inventory', validators=[DataRequired()])
-    submit = SubmitField('Add')  
+    submit = SubmitField('Add')
 
     def validate_bookname(self, bookname):
         book = Book.query.filter_by(bookname=bookname.data).first()
         if book is not None:
-            raise ValidationError(('Book already exists.')) 
+            raise ValidationError(('Book already exists.'))
+
+
+class SearchForm(FlaskForm):
+    search_string = StringField('Search', validators=[DataRequired()])
+    submit = SubmitField(('Search'))
